edd1d61c56d72643aea6b21d1be5a1362e90aa3b
MINSUNG KIM <96581415+6sung@users.noreply.github.com>
2024-08-08 15:25:57 +0900

    Update main.py

--- LawLux/main.py
+++ LawLux/main.py
@@ -1,4 +1,7 @@
 import os
+
+from clova_ocr import ocr
+
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 import json
 import base64
@@ -19,23 +22,33 @@ def index():
 @app.route('/ai_service', methods=['POST'])
 def ai_service():
     try:
-        message = request.form.get('message', '')
-        print(f"Received message: {message}")
+        input_message = request.form.get('message', '')
+        print(f"Received message: {input_message}")
 
         file = request.files.get('file')
         if file:
+            print(f"File size: {len(file.read())} bytes")
             print(f"Received file: {file.filename}")
             file_content = file.read()
             file_base64 = base64.b64encode(file_content).decode('utf-8')
             file_info = {
                 'filename': file.filename,
-                'filetype': file.content_type,
-                'filecontent': file_base64
+                'filetype': file.content_type#,
+                #'filecontent': file_base64
             }
-            #print(f"File info: {file_info}")
 
+            #print(f"File info: {file_info}")
+            message = input_message
+            #ocr_message = ocr(file)
+            #print(ocr_message)
+            #message = input_message + ocr_message
+            #message = input_message
+        else:
+            message = input_message
+
+        print(message)
         query_classify = ai_module.predict(message)
-        print(f"질문 분류 결과: {query_classify}")
+        print(f"질문 분류 결 과: {query_classify}")
 
         response_messages = {}
         if query_classify == '법률질문':
@@ -43,6 +56,24 @@ def ai_service():
             print(f"Search results: {search_results}")
             new_case_info = message
             gen_sentence = generate_sentence(search_results, new_case_info)
+            # response_search_results = [
+            #     {key: result[key] for key in result if key != '전문'}
+            #     for result in search_results
+            # ]
+
+            # response_messages = {
+            #     #'search_results': response_search_results,
+            #     'search_results': search_results,
+            #     'prompt': f"예상 형량: {gen_sentence}"
+            # }
+            def truncate_text(text, max_length=100):
+                if isinstance(text, str):
+                    return text[:max_length] + ('...' if len(text) > max_length else '')
+                return text
+
+            for result in search_results:
+                result["전문"] = truncate_text(result["전문"])
+
             response_messages = {
                 'search_results': search_results,
                 'prompt': f"예상 형량: {gen_sentence}"
